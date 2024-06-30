006d95ccdb7bd2304eca756e20a3288b31f8d5de
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2024-06-30 20:04:40 +0530

    added nats event listenr

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,5 @@
 import nats,{Message} from 'node-nats-streaming';
+import { Stan } from 'node-nats-streaming';
 
 const stan= nats.connect("ticketing","abc",{url:"http://localhost:4222"})
 
@@ -25,3 +26,39 @@ stan.on("connect",()=>{
         msg.ack();
     }) 
 })
+
+abstract class Listner {
+    abstract subject:string; // name if the channel
+    abstract queueGroupName:string; // name of the queue grp this listner will join
+    abstract onMessage(data:any,msg:Message):void ;
+    private client:Stan;
+    protected ackWait=5*1000; // 5 sec no of sec this listern has wait for ack a message
+
+    constructor(client:Stan){
+        this.client=client;
+    }
+
+    subscriptionsOptions(){
+        return this.client.subscriptionOptions().setDeliverAllAvailable().setAckWait(this.ackWait).setDurableName(this.queueGroupName).setManualAckMode(true);
+    }
+
+    listen(){
+        const subscription=this.client.subscribe(
+            this.subject,
+            this.queueGroupName,
+            this.subscriptionsOptions()
+        )
+
+        subscription.on("message",(msg:Message)=>{
+            const parsedData= this.parsePayload(msg);
+
+            this.onMessage(parsedData,msg);
+        })
+    }
+
+    parsePayload(msg:Message){
+        const payload = msg.getData();
+        return typeof payload === 'string' ? JSON.parse(payload): JSON.parse(payload.toString('utf-8'));
+    }
+}
+
